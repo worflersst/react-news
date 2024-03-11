78f825f922760022aae698e60d79edecada11650
Nikita Korchemnyi <FrontDevKN@proton.me>
2024-03-11 22:28:14 +0200

    add search unput and debounce hook

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,14 +3,20 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_API_KEY
 const API_URL = import.meta.env.VITE_BASE_API_URL
 
-export const getNews = async ({ page_number = 1, page_size = 10, category }) => {
+export const getNews = async ({
+	page_number = 1,
+	page_size = 10,
+	category,
+	keywords
+}) => {
 	try {
 		const response = await axios.get(`${API_URL}search`, {
 			params: {
 				apiKey: API_KEY,
 				page_number,
 				page_size,
-				category
+				category,
+				keywords
 			},
 		});
 		return response.data
