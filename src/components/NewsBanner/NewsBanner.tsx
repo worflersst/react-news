1fc3664c06cf18e790ae9278e61785953e708935
Nikita Korchemnyi <FrontDevKN@proton.me>
2024-04-09 13:12:28 +0200

    migrate from  js to ts

--- src/components/NewsBanner/NewsBanner.tsx
+++ src/components/NewsBanner/NewsBanner.tsx
@@ -1,8 +1,13 @@
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
+import { Inews } from "../../interfaces";
 import Image from "../Image/Image";
 import styles from "./styles.module.css";
 
-const NewsBanner = ({ item }) => {
+interface Props {
+  item: Inews;
+}
+
+const NewsBanner = ({ item }: Props) => {
   return (
     <div className={styles.banner}>
       <Image image={item?.image} />
